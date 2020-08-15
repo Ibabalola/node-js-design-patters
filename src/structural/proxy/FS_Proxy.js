50ae1d6f7d3f0614984a28aed7e9780c97f0da7e
Isaac Babalola <info@isaacbabalola.co.uk>
2020-08-15 16:38:13 +0100

    adding proxy design pattern example

---
+++ src/structural/proxy/FS_Proxy.js
@@ -0,0 +1,35 @@
+class FS_Proxy {
+
+    constructor(fs_subject) {
+        this.fs = fs_subject;
+    }
+
+    readFile(path, file_format, callback) {
+
+        if (!path.match(/.md$|.MD/)) {
+            return callback(new Error(`Can only read Markdown files.`));
+        }
+
+        this.fs.readFile(path, file_format, (error, contents) => {
+
+            if (error) {
+                console.error(error);
+                return callback(error);
+            }
+
+            return callback(contents);
+
+        });
+    }
+
+    writeFile() {
+        // clone of the fs.writeFile method
+    }
+
+    appendFile() {
+        // clone of the fs.appendFile method
+    }
+
+}
+
+module.exports = FS_Proxy;
---
+++ src/structural/proxy/index.js
@@ -0,0 +1,47 @@
+/**
+ * Structural: Proxy
+ * 
+ * A proxy is an object that controls access to another object
+ * 
+ * Intent: "Provide a surrogate or placeholder for another object to control access to it."
+ * 
+ * In addition to managing expensive objects proxies can be:
+ * - to manage a remote resource
+ * - to restrict access to the actual object
+ * - provide data validation of input
+ * - provide security
+ * - cache data
+ * - log actions
+ * 
+ * As like an adapter the proxy must provide the same interface as the object,
+ * The client would call the same method on the proxy that they would do on the 
+ * original object.
+ * 
+ * Example: Proxy (Object) that will control access to the file system
+ */
+
+const path = require('path');
+const FS_Proxy = require('./FS_Proxy');
+
+// the proxy emulates the interface of actual fs module 
+// however has additional functionailty 
+// guard the reading of the txt file
+const fs = new FS_Proxy(require('fs'));
+
+const txtFile = path.join(__dirname, 'Readme.txt');
+const mdFile = path.join(__dirname, 'Readme.md');
+
+const result = (error, contents) => {
+
+    if (error) {
+        console.log('\x07');
+        console.error(error);
+        process.exit(0);
+    }
+
+    console.log('reading file...');
+    console.log(contents);
+}
+
+fs.readFile(txtFile, 'UTF-8', result);
+fs.readFile(mdFile, 'UTF-8', result);
